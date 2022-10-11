0525612042b4914c7ed2def064e352d00a268e7f
pawardipali546 <107623648+pawardipali546@users.noreply.github.com>
2022-10-11 10:50:38 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Se
